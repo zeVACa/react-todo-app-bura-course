d27083efaf26a3006bfff303ac8db9ed5c3ac21a
zeVACa <logunik@bk.ru>
2021-01-09 02:09:37 +0300

    isOdd - props practice

--- src/comptonents/todo-item.js
+++ src/comptonents/todo-item.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
-const ListItem = (props) => {
+const ListItem = ({ num, isImportant = false }) => {
+
+   // props.isImportant = props.num % 2 ? true : false
+
+   isImportant = num % 2 ? true : false;
+
+   const style = {
+      color: isImportant ? 'tomato' : 'gray'
+   }
+
+   let isOdd = (num % 2) ? 'odd' : 'even';
+
    return (
-      <span>item {props.num}</span>
+      <span style={style}>{`${num} is ${isOdd} item`}</span>
+
    )
 }
 
